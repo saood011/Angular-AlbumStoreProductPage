c9824eee69bf49959dfcf6a84d6f300558a91d85
Mohd Saood Akhtar <48763014+saood011@users.noreply.github.com>
2022-07-12 12:23:12 +0200

    update

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,7 +8,7 @@ import { Product } from './product';
 @Injectable()
 export class ProductService {
   private _albumUrl = "../assets/album.json";
-  private _productUrl = "../assets/products.json";
+  private _productsUrl = "../assets/products.json";
 
   constructor(private _http:Http) { }
 
@@ -16,6 +16,6 @@ export class ProductService {
    return this._http.get(this._albumUrl).map((response) => <Album>response.json())
   }
   getProducts(): Observable<Product[]>{
-    return this._http.get(this._productUrl).map((response) => <Product[]>response.json())
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json())
    }
 }
